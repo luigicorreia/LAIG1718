5798e7b7d7a96592c00feca1afbda40c9fd1cbe8
Vicente Espinha <up201503764@fe.up.pt>
2017-10-22 21:01:05 +0100

    triangulos corrigidos

--- MyTriangle.js
+++ MyTriangle.js
@@ -3,73 +3,66 @@
  * @constructor
  */
   function MyTriangle(scene,args) {
- 	CGFobject.call(this,scene);
+ 	/*CGFobject.call(this,scene);
 
- 	this.initBuffers(args);
+ 	this.initBuffers(args);*/
+  CGFobject.call(this,scene);
+
+	this.args = args;
+	this.initBuffers();
  };
 
  MyTriangle.prototype = Object.create(CGFobject.prototype);
  MyTriangle.prototype.constructor = MyTriangle;
 
- MyTriangle.prototype.initBuffers = function(args) {
+ MyTriangle.prototype.initBuffers = function() {
 
-   var coordenates = args.split(" ").map(Number);
+   var coordenates = this.args.split(" ");
 
+   //lados do triangulo
+   this.a = Math.sqrt(Math.pow(coordenates[0] - coordenates[6], 2) + Math.pow(coordenates[1] - coordenates[7], 2) + Math.pow(coordenates[2] - coordenates[8], 2));
+   this.b = Math.sqrt(Math.pow(coordenates[3] - coordenates[0], 2) + Math.pow(coordenates[4] - coordenates[1], 2) + Math.pow(coordenates[5] - coordenates[2], 2));
+   this.c = Math.sqrt(Math.pow(coordenates[6] - coordenates[3], 2) + Math.pow(coordenates[7] - coordenates[4], 2) + Math.pow(coordenates[8] - coordenates[5], 2));
 
- 	this.vertices = [
- 	coordenates[0], coordenates[1], coordenates[2],
- 	coordenates[3], coordenates[4], coordenates[5],
- 	coordenates[6], coordenates[7], coordenates[8],
- 	];
+  //cosb e sinb
+  this.cos_b = Math.cos((Math.pow(this.a, 2) - Math.pow(this.b, 2) + Math.pow(this.c, 2))/(2 * this.b * this.c));
+  this.sin_b = Math.sqrt(1 - Math.pow(this.cos_b, 2));
 
- 	this.indices = [
- 	0, 1, 2,
- 	];
-  var v1 = vec3.fromValues(coordenates[0], coordenates[1], coordenates[2]);
-	var v2 = vec3.fromValues(coordenates[3], coordenates[4], coordenates[5]);
-	var v3 = vec3.fromValues(coordenates[6], coordenates[7], coordenates[8]);
+  this.vertices = [
+          coordenates[0], coordenates[1], coordenates[2],
+          coordenates[3], coordenates[4], coordenates[5],
+          coordenates[6], coordenates[7], coordenates[8],
+    ];
 
-  this.AB = vec3.create();
-	vec3.sub(this.AB,v2, v1);
-	this.AC = vec3.create();
-	vec3.sub(this.AC, v3, v1);
-	this.BC = vec3.create();
-	vec3.sub(this.BC, v3, v2);
+  this.indices = [
+    0, 1, 2
+  ];
 
-	var N = vec3.create();
-	vec3.cross(N, this.AB, this.BC);
-	vec3.normalize(N, N);
-
-	this.normals = [
-		N[0], N[1], N[2],
-		N[0], N[1], N[2],
-		N[0], N[1], N[2],
+  this.normals = [
+      0, 1, 0,
+      0, 1, 0,
+      0, 1, 0
     ];
 
 
-  var tC = (vec3.sqrLen(this.AB) + vec3.sqrLen(this.AC) - vec3.sqrLen(this.BC))/ (vec3.length(this.AB) * 2);
-	var sC = Math.sqrt(vec3.sqrLen(this.AC) - tC * tC);
-  
   this.originalTexCoords = [
-		0,0,
-		vec3.length(this.AB),0,
-    sC,tC
-	];
+      0, 0,
+      0, 1,
+      1, 1
+    ];
 
-	this.texCoords = this.originalTexCoords.slice();
+  this.texCoords = this.originalTexCoords.slice();
 
- 	this.primitiveType = this.scene.gl.TRIANGLES;
- 	this.initGLBuffers();
+this.primitiveType=this.scene.gl.TRIANGLES;
+this.initGLBuffers();
  };
 
  MyTriangle.prototype.scaleTexCoordss = function(s, t) {
 
-var cosB = vec3.dot(this.AB,this.BC)/(vec3.length(this.AB)*vec3.length(this.BC));
-
-this.texCoords = [
-  0,1,
-  vec3.length(this.AB)/s,1,
-  (vec3.length(this.AB)-vec3.length(this.BC)*cosB)/s, (1-vec3.length(this.BC)*Math.sqrt(1-(Math.pow(cosB,2))))/t
-];
- 	this.updateTexCoordsGLBuffers();
+  this.texCoords = [
+			0, 1,
+			this.c/s, 1,
+			(this.c - this.a * this.cos_b)/s, (1 - this.a * this.sin_b)/(t)
+		];
+	this.updateTexCoordsGLBuffers();
 }
