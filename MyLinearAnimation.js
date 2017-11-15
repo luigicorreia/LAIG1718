d95475d9b84a1667d42cee6f13f14c79405d7308
Luís Correia <luis.mcorreia97@gmail.com>
2017-11-15 10:51:21 +0000

    beginning shaders

--- MyLinearAnimation.js
+++ MyLinearAnimation.js
@@ -3,29 +3,29 @@
 * @constructor
 */
 
-function MyLinearAnimation(scene,controlPoints,speed) extends MyAnimation{
-  MyAnimation.call(this);
+function MyLinearAnimation(scene, id, speed, controlPoints) {
+  this.scene = scene;
+  this.id = id;
+  this.controlPoints = controlPoints;
   this.distance = 0;
-  this.speed = speed;
 
-
-  for(var i = 0; i < controlPoints.length-1; i++){
-    distance += calculateDistance(controlPoints[i], controlPoints[i+1]);
+  for(var i = 0; i < this.controlPoints.length-1; i++){
+    this.distance += this.calculateDistance(this.controlPoints[i], this.controlPoints[i+1]);
   }
 
   this.time = this.distance / this.speed;
-
 }
 
 MyLinearAnimation.prototype = Object.create(MyAnimation.prototype);
 MyLinearAnimation.prototype.constructor = MyLinearAnimation;
+
 
 MyLinearAnimation.prototype.display = function(){
   console.log(distance);
 }
 
 MyLinearAnimation.prototype.calculateDistance = function(p0, p1){
-  Math.sqrt(
+  return Math.sqrt(
     Math.pow((p1[0] - p0[0]), 2) + Math.pow((p1[1] - p0[1]), 2) + Math.pow((p1[2] - p0[2]), 2)
   );
 }
